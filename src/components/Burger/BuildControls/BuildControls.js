31abfaefa10559b66d5bface5fbe4fee40e58072
Remigiusz Kolanko <remigiusz.kolanko@gmail.com>
2019-09-05 21:37:39 +0200

    Burger and BildControles styles created

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import classes from './BuildControls.module.css';
+
+const buildControls = () => (
+    <div>
+    </div>
+);
+
+export default buildControls;
---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+
+    let ingredients = Object.keys(props.ingredients)
+        .map(ingredientKey => {
+            return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+                return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+            })
+        })
+        .reduce((prev, curr) => {
+            return prev.concat(curr)
+        }, []);
+
+    if (!ingredients.length) {
+        ingredients = <p>Please start adding ingredients!</p>
+    }
+
+    console.log(ingredients);
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top"/>
+                {ingredients}
+            <BurgerIngredient type="bread-bottom"/>
+        </div>
+    );
+}
+
+export default burger;
