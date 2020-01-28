006323f533bc036bfa662ed143451562c1ea6f4c
Remigiusz Kolanko <remigiusz.kolanko@synergycodes.com>
2020-01-28 07:41:48 +0100

    autentication implemented

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -10,11 +10,10 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 class Orders extends Component {
 
     componentDidMount() {
-        this.props.onFetchOrders(this.props.token);
+        this.props.onFetchOrders(this.props.token, this.props.userId);
     }
 
     render() {
-        console.log(this.props.orders);
         let orders = <Spinner />;
         if (!this.props.loading) {
             orders = this.props.orders.map(order => (
@@ -37,13 +36,14 @@ const mapStateToProps = state => {
     return {
         orders: state.order.orders,
         loading: state.order.loading,
-        token: state.auth.token
+        token: state.auth.token,
+        userId: state.auth.userId
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onFetchOrders: (token) => dispatch(actions.fetchOrders(token))
+        onFetchOrders: (token, userId) => dispatch(actions.fetchOrders(token, userId))
     };
 };
 
